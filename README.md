91d072a481a0081b952ad15c22f237d096ffa851
Joshua Suskalo <joshua@suskalo.org>
2021-05-22 15:53:00 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Skitter
+
+## License
+
+Copyright © 2021 Joshua Suskalo
+
+Distributed under the Eclipse Public License version 1.0.
---
+++ test/skitter/core_test.clj
@@ -0,0 +1,7 @@
+(ns skitter.core-test
+  (:require [clojure.test :refer :all]
+            [skitter.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
