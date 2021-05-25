28b07196e4604580795ab0ad999b47bdd9c01d78
Joshua Suskalo <joshua@suskalo.org>
2021-05-25 08:49:30 -0500

    Separate builtins to two macros for convenience

--- src/skitter/builtins.clj
+++ src/skitter/builtins.clj
@@ -2,24 +2,31 @@
   (:require
    [skitter.runtime :as r]))
 
-(defmacro defbuiltin
-  [name reexport? cont-sym arglist & body]
+(defmacro defcall
+  [name reexport? arglist & body]
   `(do
      (defmethod r/call '~name
-       [cont# _# args#]
-       (-> cont#
-           (update ::value-stack
-                   conj [:val (let [~cont-sym cont#
-                                    ~arglist args#]
-                                ~@body)])
-           (update ::ns conj nil)))
-     (swap! r/global-env assoc-in '[skitter.lang ::ns-map ~name] '~name)
-     (swap! r/global-env update-in '[skitter.lang ::ns-publics] (fnil conj #{}) '~name)
+       ~arglist
+       ~@body)
+     (swap! r/global-env assoc-in '[skitter.lang ::r/ns-map ~name] '~name)
+     (swap! r/global-env update-in '[skitter.lang ::r/ns-publics] (fnil conj #{}) '~name)
      ~(if reexport?
         `(do
-           (swap! r/global-env assoc-in '[skitter.core ::reexports ~name] 'skitter.lang)
-           (swap! r/global-env update-in '[skitter.core ::ns-publics] (fnil conj #{}) '~name))
-        `(swap! r/global-env assoc-in '[skitter.core ::ns-map ~name] '~name))))
+           (swap! r/global-env assoc-in '[skitter.core ::r/reexports ~name] 'skitter.lang)
+           (swap! r/global-env update-in '[skitter.core ::r/ns-publics] (fnil conj #{}) '~name))
+        `(swap! r/global-env assoc-in '[skitter.core ::r/ns-map ~name] '~name))
+     '~name))
+
+(defmacro defbuiltin
+  [name reexport? cont-sym arglist & body]
+  `(defcall ~name ~reexport?
+     [cont# _# args#]
+     (-> cont#
+         (update ::r/value-stack
+                 conj [:val (let [~cont-sym cont#
+                                  ~arglist args#]
+                              ~@body)])
+         (update ::r/ns conj nil))))
 
 (defbuiltin do true
   _ [& args]
@@ -41,9 +48,9 @@
   cont [export-map public?]
   (run! (fn [[ns syms]]
           (run! (fn [sym]
-                  (swap! r/global-env assoc-in [(first (::ns cont)) ::reexports sym] ns)
+                  (swap! r/global-env assoc-in [(first (::r/ns cont)) ::r/reexports sym] ns)
                   (when public?
-                    (swap! r/global-env update-in [(first (::ns cont)) ::ns-publics] conj sym)))
+                    (swap! r/global-env update-in [(first (::r/ns cont)) ::r/ns-publics] conj sym)))
                 syms))
         export-map)
   nil)
@@ -51,8 +58,8 @@
 (defbuiltin reexport-all true
   cont [export-ns public?]
   (run! (fn [sym]
-          (swap! r/global-env assoc-in [(first (::ns cont)) ::reexports sym] export-ns)
+          (swap! r/global-env assoc-in [(first (::r/ns cont)) ::r/reexports sym] export-ns)
           (when public?
-            (swap! r/global-env update-in [(first (::ns cont)) ::ns-publics] conj sym)))
-        (get-in @r/global-env [export-ns ::ns-publics]))
+            (swap! r/global-env update-in [(first (::r/ns cont)) ::r/ns-publics] conj sym)))
+        (get-in @r/global-env [export-ns ::r/ns-publics]))
   nil)
